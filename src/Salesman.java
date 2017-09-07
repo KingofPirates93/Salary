9a62c039f9f7a2bf8923fa09821dadfd893090ab
Unknown <swapnilp618@gmail.com>
2017-09-06 21:45:20 -0400

    Finished two classes

--- src/Salesman.java
+++ src/Salesman.java
@@ -6,5 +6,43 @@
  * Author: Swapnil Patel
  * Date: 9/6/2017
  */
-public class Salesman {
+public class Salesman extends Employee {
+    //global var
+    int soldItems;
+
+    //constructor
+    public Salesman(String NAME, int MONSALARY, int SoldItems) {
+        super( NAME, MONSALARY );
+        this.soldItems = SoldItems;
+    }
+
+    //Yearly salary
+    public int annSalesmanSalary(){
+        int COMMISSION = 0;
+        int BONUSRATE = 2;
+        int BASEPAY = super.annualSalary();
+
+        if (soldItems >= 300)
+        {
+            COMMISSION = BONUSRATE * getMonthlySalary();
+        }
+        else if (soldItems > 200 && soldItems < 300 )
+        {
+            COMMISSION += getMonthlySalary();
+        }
+
+        else if (soldItems < 200)
+        {
+            COMMISSION = 0;
+        }
+
+        return BASEPAY + COMMISSION;
+
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + "Number of items sold: " + soldItems + "\n";
+    }
+
 }
