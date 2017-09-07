99b766261bcc414a5668c4177feb7c26191bf229
Unknown <swapnilp618@gmail.com>
2017-09-06 22:39:16 -0400

    added stuff

--- src/P1Driver.java
+++ src/P1Driver.java
@@ -11,8 +11,35 @@ import javax.swing.*;
 import java.io.*;
 public class P1Driver {
 
-    public static void main(String[] args) {
+
+    //variable initialization
+    String TEMP = " ";
+    private static final String FILENAME = "inputData.txt";
+    private static ArrayList <Employee> Employees2015;
+    private static ArrayList <Employee> Employees2016;
+    private static String[] Employee;
+
+    public static void main(String[] args) throws IOException {
+
+        Employees2015 = new ArrayList<Employee>();
+        Employees2016 = new ArrayList<Employee>();
+        BufferedReader bufferedReader = new BufferedReader( new FileReader( FILENAME ) );
+
+        String LINE = bufferedReader.readLine();
+
+            while (LINE != null) {
+                
+
+                LINE = bufferedReader.readLine();
+
+            }
+
+
+
+
+
 
     }
+
 
 }
